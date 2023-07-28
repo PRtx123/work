5f241934f6b3237902efa5f2075996a4ffd9b2b9
Сидоров Вадим Алексеевич <VASidorov@t1-consulting.ru>
2023-07-28 11:02:35 +0300

    v5

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        
-        System.out.println("Hello");
-        System.out.println("1");
-        
-        System.out.println("2");
-     
-        System.out.println("1");
-
+       
     }
 }
