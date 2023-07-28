6e96584440f947e818c4ef958b80299404c4267f
Сидоров Вадим Алексеевич <VASidorov@t1-consulting.ru>
2023-07-28 10:54:35 +0300

    dasdsad

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        
         System.out.println("Hello");
         System.out.println("1");
         
         System.out.println("2");
-        System.out.println("3");
+     
         System.out.println("1");
 
     }
