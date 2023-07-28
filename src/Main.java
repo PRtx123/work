5a827af18836eb66d7ba8f002716222356c9a1d5
Сидоров Вадим Алексеевич <VASidorov@t1-consulting.ru>
2023-07-28 09:42:23 +0300

    chng

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        int b = 11;
-        System.out.println("a");
-        System.out.println("b");
+        
         System.out.println("Hello");
         System.out.println("1");
-        System.out.println("1");
+        
+        System.out.println("2");
+        System.out.println("3");
         System.out.println("1");
 
     }
