da5e9ef35072bb48c380644a8846f65b22209ff6
Сидоров Вадим Алексеевич <VASidorov@t1-consulting.ru>
2023-07-31 23:42:34 +0300

    v11

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = 11;
+        int h = 1000;
         String email = "Test@test.ru"
         System.out.println("b");
         
