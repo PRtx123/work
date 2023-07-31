661db20175f8b734beae2bb2fde625a0069df81d
Сидоров Вадим Алексеевич <VASidorov@t1-consulting.ru>
2023-07-31 16:50:01 +0300

    v1

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         int a = 10;
         int b = 11;
         System.out.println("a");
-        System.out.println("b");
+        
         System.out.println("Hello");
         System.out.println("1");
+        System.out.println("2");
         System.out.println("1");
-        System.out.println("1");
-
+        
     }
 }
