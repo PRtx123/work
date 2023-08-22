a93dc88809b174aedfc515ed9b3b9daefbe7b07f
Сидоров Вадим Алексеевич <VASidorov@t1-consulting.ru>
2023-08-22 15:13:07 +0300

    dasdsad

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int a ;
         int b = 11; int c = 8;
-        int h = 1000;
         String email = "Test.ru";
         int t = 11;
         int e = 8;
