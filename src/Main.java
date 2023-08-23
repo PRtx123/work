5dfd1de3aa6e43b3e34c506d500fc3fb27471bac
PRtx123 <51357018+PRtx123@users.noreply.github.com>
2023-08-23 12:11:12 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
         int t = 11;
         int e = 8;
         int f = 7;
-
+        int x = 2;
+        
         public void bad(){
             while(true){
                 System.out.println("bad");
