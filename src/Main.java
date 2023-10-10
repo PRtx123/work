2926c46c561f01acf525bfd046136d75f92db9f2
KseniyaIgnatenko <116016509+KseniyaIgnatenko@users.noreply.github.com>
2023-10-10 16:43:22 +0300

    Update Main.java
    впып

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        int b = 11;
         System.out.println("a");
         System.out.println("b");
         System.out.println("Helllo");
