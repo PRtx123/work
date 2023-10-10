f1c4605a955998ae75cb48780131d95939834f22
KseniyaIgnatenko <116016509+KseniyaIgnatenko@users.noreply.github.com>
2023-10-10 16:55:31 +0300

    Update Main48.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
+        int b = 100;
         System.out.println("a");
         System.out.println("b");
         System.out.println("Helllo");
