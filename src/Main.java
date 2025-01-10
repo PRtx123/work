91949686a24d6f15303e1703b5f118cbb7ecad86
PRtx123 <51357018+PRtx123@users.noreply.github.com>
2025-01-10 18:02:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         3
-        System.out.println("a");
+        //System.out.println("a");
         System.out.println("b");
         System.out.println("Helllo");
         System.out.println("a");
