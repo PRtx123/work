7ec0b57914d4355048522f5b7bc3b06fc6dcba86
PRtx123 <51357018+PRtx123@users.noreply.github.com>
2025-04-11 09:16:48 +0300

    Add files via upload

---
+++ src/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 0;
+        int c = 0;
+        int g = 7;
+
+        int m = 7;
+    
+    }
+}
